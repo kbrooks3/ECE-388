f8370dfa075b1bd9647f031214c923a0f51b437b
kbrooks3 <55256244+kbrooks3@users.noreply.github.com>
2019-11-05 13:21:25 -0500

    Update main.cpp

--- LCD388/LCD388/main.cpp
+++ LCD388/LCD388/main.cpp
@@ -1,8 +1,6 @@
 /*
- * Digital Tape Measure 2.c
- *
- * Created: 4/17/2019 12:52:39 PM
- * Author : kbrooks3
+Updated 11/5/19 to add all of the code for the peripherals.
+The only glaring thing that needs to be edited is the 
  */ 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,54 +9,75 @@
 #define F_CPU 16000000
 #include <util/delay.h>
 
+#define LEFT 3200 	//timer length
+#define RIGHT 2400 	//timer length
+#define CENTER 2800 	//timer length
+#define DSCALER		//still need to find a scalar
+#define TOLERANCE 0.025 //inches
+
 void lcdinitialize();
 void lcdcommand(char cmnd);
 void lcddata(char data);
 void lcdprint(char * str );
 void lcd_gotoxy(char x, char y);
+void tilt(int deltax);
+void angle(int pulseLength);
+void sensortrigger();
 
-int val;
-int dig;
+volatile uint32_t actual; //inches
+volatile uint32_t targetx; //inches
+int val;			//current variable for distance, needs to be changed
 char x, y;
 char strtarget[20];
 char stractual[20];
-char scl[4];
+volatile uint32_t count = 0;
+volatile uint32_t button = 0;
+volatile uint32_t scale = 1; //change to data type with better precision 
+char SCL[4];
+
 
 int main(void)
 {
    	DDRD = DDRD | 0b11110000;	//d4-7 output to screen
    	//DDRB = DDRB | 0b11111111;	//NEEDS TO BE CHANGED FOR ENCODER AND SERVO
-   	DDRC = DDRC | 0b00010111;	//c[0-2] (RS, RW, and E) and 4 (Trig) set to output
+   	DDRB = (DDRB & 0b11111010); 	//set b2 and b0 to input, connected to CLK and DT
+	DDRC = DDRC | 0b00010111;	//c[0-2] (RS, RW, and E) and 4 (Trig) set to output
 	DDRC = DDRC & 0b11010111;	//c3 (Switch) and 5 (Echo) set to input
 
-	//Timer for sensor
-	TCCR1A = (0b00<<COM1A0)|(0b00<<COM1B0)|(0b00<<WGM10);	//Normal Mode
-	TCCR1B = (0b0<<WGM12)|(0b010<<CS10);			//clock scalar by 8
-	TIMSK1 = (0b0<<TOIE1);					//disable interrupts for timer1
+	PCICR |= (1<<PCIE0); 
+    	PCMSK0 |= (1<<1); //Pin B1 interrupt
+    	PCICR |= (1<<PCIE1);
+    	PCMSK1 |= (1<<4); //Pin C4 interrupt
+
+	//Timer for sensor, changing from 1 to 3
+	TCCR3A = (0b00<<COM3A0)|(0b00<<COM3B0)|(0b00<<WGM30);	//Normal Mode
+	TCCR3B = (0b0<<WGM32)|(0b010<<CS30);			//clock scalar by 8
+	TIMSK3 = (0b0<<TOIE3);					//disable interrupts for timer3
 	
+	//Timer for PWM
+	TCCR1A = (0b10 << COM1A0) | (0 << COM1B0) | (0b10 <<  WGM10); //
+	TCCR1B = (0b11 << WGM12) | (0b010 << CS10);
+	ICR1 = 40000; 						// 20ms with /8 prescalar
+	OCR1A = 1000; 						// 1000 -> 4000 (0.5 ms - 2 ms) // sets the duty cycle
+
 	sei();							//C function to enable global interrupts
 	
     while (1) 
     {
 	sprintf(strtarget,"%.4d in SCL",val);			//val becomes a string
-	//The line below needs to be changed to include a character string for scl
-	//Something like sprintf(stractual,"%.4d in %c", val, scl); 
-	sprintf(stractual,"%.4d in WUT",val);			//val becomes a string
+	sprintf(stractual,"%.4d in %c",actual, SCL);			//val becomes a string
 	lcdinitialize();
 	lcd_gotoxy(1,1);						//write on the first line
 	lcdprint(strtarget);							//that string is sent to the LCD
 	lcd_gotoxy(1,2);						//write on the second line
 	lcdprint(stractual);
-
+	sensortrigger();
 	
 	
-		PORTC = 0b00100000;
-		_delay_us(10);
-		PORTC = 0b00000000;
-		
+			
 		while ((PINC&(0b00010000))==0b00000000)
 		{
-			TCNT1 = 0;
+			TCNT3 = 0;
 		}
 		while ((PINC&(0b00010000))==0b00010000)
 		{
@@ -66,22 +85,28 @@ int main(void)
 		}
 		//These values need to be changed to account for the increase in resolution and faster clock
 	    	
-		if (TCNT1 >= 2664)
+		if (TCNT3 >= 2664)
 		{
-			val = 9999;
+			actual = 9999;
 		}
 		else
 		{
-			val = TCNT1/37;
+			actual = TCNT3/37;
 		}
 	
-    
+    	deltax = target - actual;
+	tilt(deltax);
 	_delay_ms(10);
 	}
 }
 
 
-
+void sensortrigger()
+{
+	PORTC = 0b00100000;	//trigger for sensor
+	_delay_us(10);
+	PORTC = 0b00000000;	//turn off trigger for sensor
+}
 
 void lcdinitialize()	//starts the LCD
 {
@@ -141,4 +166,79 @@ void lcddata(char data)
 	PORTC = 0b00000001;
 	_delay_us(100);
 	
+}
+
+ISR(PCINT0_vect) //Pin B2 (rotation) interrupt
+{
+    if ((PINB & 0b00000100) == 0b00000000) //check falling edge
+    {
+        if ((PINB & 0b00000001) == 0b00000000) //counterclockwise
+        {
+            target = target - scale; //decrement by scale
+        }
+        else //clockwise
+        {
+            target = target + scale; //increment by scale
+        }
+    }
+}
+
+ISR(PCINT1_vect) //Pin C3 (button) interrupt
+{
+    if ((PINC & 0b00001000) == 0b00000000)
+    {
+        button = 1; // button is pressed
+        // Change scale based on previous scale
+        If (scale == 1){
+            	scale = 0.25; 	// 1 / 4 inch scale
+            	SCL = “1/4";
+        }
+        else if (scale == 0.25){
+            	scale = 0.025; 	// 1 / 40 inch scale
+            	SCL = “1/40";
+        }
+        else if (scale == 0.025){
+            	scale = 1; 	// 1 inch scale
+		SCL = “1”;
+        }
+    }
+    else
+    {
+        button = 0;
+    }
+}
+
+//Functions for Servo follow
+
+void tilt(int deltax)
+{
+    if (deltax > 0)
+    {
+        angle(RIGHT)
+        _delay_ms(abs(deltax)*DSCALER)
+        angle(CENTER)
+    }
+    
+    if (deltax < 0)
+    {
+        angle(LEFT)
+        _delay_ms(abs(deltax)*DSCALER)
+        angle(CENTER)
+    }
+}
+
+void angle(int pulseLength)
+{
+   if (pulselength == LEFT)
+   {
+	   OCR1A = LEFT;	//change top value of timer to 3200 for 1.6 ms
+   }
+	else if (pulselength == RIGHT)
+	{	
+		OCR1A = RIGHT;	//change top value of timer to 2400 for 1.2 ms
+	}
+	else
+	{
+		OCR1A = CENTER;	//change top value of timer to 2800 for 1.4 ms
+	}
 }
